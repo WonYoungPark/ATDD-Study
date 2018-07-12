decce6e81cb8edaa8b03a35eb68e9a01aa6bfa31
One.0 <wyparks2@mrblue.com>
2018-07-12 20:02:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## ATDD(Acceptance Test Driven Development)
+인수 테스트 주도 개발 스터디
---
+++ src/main/java/io/github/wonyoungpark/atddstudyexample/AtddStudyExampleApplication.java
@@ -0,0 +1,12 @@
+package io.github.wonyoungpark.atddstudyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddStudyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddStudyExampleApplication.class, args);
+    }
+}
